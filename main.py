4ac6be8b637865669b58ac09fe90f268213f4383
Marcin <marcinzag@yahoo.com>
2021-04-15 00:44:57 +0200

    homework done update

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from typing import Optional
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 import hashlib
 import datetime
 
-from starlette.status import HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND
+from starlette.status import HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND, HTTP_422_UNPROCESSABLE_ENTITY
 
 class NewUser(BaseModel):
     name: str
@@ -27,14 +27,15 @@ def root():
     return {"message": "Hello world!"}
 
 @app.get("/auth")
-async def check_login(password: str, password_hash: str):
+async def check_login(password: str = '', password_hash: str = ''):
     hash = hashlib.sha512()
     hash.update(password.encode('utf-8'))
+    if password == '' or password_hash == '':
+        return Response(status_code=HTTP_401_UNAUTHORIZED)
     if hash.hexdigest() == password_hash:
         return Response(status_code=HTTP_204_NO_CONTENT)
     else:
         return Response(status_code=HTTP_401_UNAUTHORIZED)
-
 @app.post("/register", status_code=201)
 async def new_user(user: NewUser):
     app.users.append(RegisteredUser(name=user.name, surname=user.surname))
