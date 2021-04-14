35f60578c083ef1882b19fc4381f3316be3f72cb
Marcin <marcinzag@yahoo.com>
2021-04-15 00:05:09 +0200

    homework done

--- tests.py
+++ tests.py
@@ -1,4 +1,7 @@
+from fastapi import responses
 from fastapi.testclient import TestClient
+import pytest
+import json
 
 from main import app
 
@@ -7,4 +10,18 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World!"}
+    assert response.json() == {"message": "Hello world!"}
+
+def test_methods():
+    response = client.delete("/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "DELETE"}
+
+def test_auth():
+    response = client.get("/auth", params={"password": "siema", "password_hash": "638b2488c3e94d6d6f73f8e875f99aa62325152885aa81441b8315245264cebf2b090b60cfe7591a892798094809437915061cb9dd939d6cd35e6a370df669f3"})
+    assert response.status_code == 204
+
+def test_signup():
+    response = client.post("/register", data=json.dumps({"name": "Siurek", "surname": "Wiórek"}))
+    assert response.status_code == 200
+    assert response.json() == {"name": "Siurek", "surname": "Wiórek"}
